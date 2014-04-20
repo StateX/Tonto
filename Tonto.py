1f9002745eba4532b93cb705d2d5d663ed5e4844
StateX <statexblog@gmail.com>
2014-04-20 21:32:00 +0200

    +modulos(banco_ideas y programacición)

--- Tonto.py
+++ Tonto.py
@@ -3,8 +3,10 @@
 import os
 import sys 
 import time
+from datetime import date 
 
 version = "0.0.0.1"
+ideas = []
 
 def trabajo(seleccion):	
 	print "Accediendo a banco de trabajo"
@@ -64,6 +66,71 @@ def musica(beethoven):
 		time.sleep(3)	
 		sys.exit(0)
 
+def programar(Wozniak):
+	 print "Accediendo a su editor de codigo favorito"
+	 os.system('"C:\Users\Albert\Downloads\Jorge13.wav"')
+	 os.system('"C:\Program Files\Sublime Text 2\sublime_text.exe"')
+	 time.sleep(3)
+	 print "Necesitas algo mas? "
+	 os.system('"C:\Users\Albert\Downloads\Jorge7.wav"')
+	 print "[s = si] [n = no]"
+	 d2 =  raw_input("-->:")
+	 if d2 == "s":
+		 os.system('cls')
+	 elif d2 == "n":
+		 os.system('cls')
+		 print "que le vaya bien el dia señor"
+		 os.system('"C:\Users\Albert\Downloads\Jorge1.wav"')
+		 time.sleep(3)	
+		 sys.exit(0)
+
+def banco(delacalle):
+	 while True:
+	 	 recogedor = open('banco_ideas.txt','a+')
+	 	 for item in recogedor:
+	 	 	 ideas.append(item)	 
+	 	 print "estas son las ideas que ha recogido hasta ahora."
+	 	 os.system('"C:\Users\Albert\Downloads\Jorge14.wav"')
+	 	 time.sleep(3)
+	 	 print ideas
+	 	 os.system('"C:\Users\Albert\Downloads\Jorge15.wav"')
+	 	 idea = raw_input("Que se le ha ocurrido?:  ")
+	 	 ideas.append(idea)
+	 	 ideas.append(date.today())
+	 	 print ideas
+	 	 time.sleep(3)
+	 	 os.system('"C:\Users\Albert\Downloads\Jorge16.wav"')
+	 	 d3 = raw_input("Ajunto otra idea señor?: ") 
+		 if d3 == "s":
+		 	 os.system('cls')
+	 		 print idea
+	 	 elif d3 == "n":
+	 	 	 i = str(ideas)
+	 	 	 banco_ideas = open('banco_ideas.txt','a+')
+	 	 	 print ideas
+		 	 banco_ideas.write(i)
+		 	 banco_ideas.close()
+		 	 os.system('"C:\Users\Albert\Downloads\Jorge17.wav"')
+		 	 print "ideas guardadas con exito"
+			 time.sleep(3)
+			 print "Algo mas señor? "
+	 	 	 os.system('"C:\Users\Albert\Downloads\Jorge8.wav"')
+	 	 	 print "[s = si] [n = no]"
+	 	 	 d2 =  raw_input("-->:")
+	 	 	 if d2 == "s":
+		 	 	 os.system('cls')
+		 	 	 break
+	 	 	 elif d2 == "n":
+		 	 	 os.system('cls')
+		 	 	 print "que tenga un buen dia señor"
+		 	 	 os.system('"C:\Users\Albert\Downloads\Jorge2.wav"')
+		 	 	 time.sleep(3)
+		 	 	 sys.exit(0)
+	 	 else: 
+	 	 	 print "selecione una de las 2 señor"
+	 	 	 os.system('"C:\Users\Albert\Downloads\Jorge18.wav"')
+	 	 	 time.sleep(5)
+
 def games(juegos):
 	 print "Iniciando Ragnarock Online, ya que tiene los juegos desordenados por el disco duro."
 	 os.system('"C:\Users\Albert\Downloads\Jorge12.wav"')
@@ -110,13 +177,17 @@ def fin(apagar):
 print "Hola Albert, que te apeteceria hacer hoy?" 
 os.system('"C:\Users\Albert\Downloads\Jorge.wav"')
 time.sleep(3)
+
 #Antes de esto me podria dar la tempertura de mi region. Tendre que mirarlo
 #estoy ira así facil y comodo xD
+
 while True:
 	 print"""
 [a = Me apetece, trabajar] 
-[b= bah, tendre que estudiar]
+[b = bah, tendre que estudiar]
 [c = Musica, Animame el dia] 
+[p = programar]
+[i = ideas]
 [d = Jugar] 
 [e = Quien eres?] 
 [f = No requiero tu ayuda, gracias]	
@@ -129,6 +200,10 @@ while True:
 	 	 print estudiar(d1)	 
 	 elif d1 == "c": 
 	 	 print musica(d1)	
+	 elif d1 == "p": 
+	 	 print programar(d1)
+	 elif d1 == "i": 
+	 	 print banco(d1)	 	 
 	 elif d1 == "d":
 	 	 print games(d1)	  
 	 elif d1 == "e":
